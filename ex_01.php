e9245783830cd20abdc09696da35b61f26d75a30
SenczuckCode <lucas_senczuck@estudante.sesisenai.org.br>
2026-07-06 07:47:10 -0300

    up - ex-1-feito

---
+++ ex_01.php
@@ -0,0 +1,15 @@
+<?php
+// (x² + y²) / (x + y)
+function calcularFormula ($x, $y)
+{
+if (($x + $y) == 0){
+return "Não é possível realizar a divisão por zero.";
+}
+$resultado = (pow($x, 2) + pow($y, 2)) / ($x + $y);
+return $resultado;
+}
+$x = 10;
+$y = 5;
+echo "Valor de X: $x <br>";
+echo "Valor de Y: $y <br><br>";
+echo "Resultado: ". calcularFormula ($x, $y);
